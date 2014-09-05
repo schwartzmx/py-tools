f5985870f7f1cd5fe979dcc4f6528b6113256cc8
Phil Schwartz <schwartzmx@gmail.com>
2014-09-04 21:10:59 -0500

    It works :], still testing Directories

--- cryptdir.py
+++ cryptdir.py
@@ -12,66 +12,110 @@ from Crypto.Hash import SHA256
 
 
 def encrypt(key, file):
-
-    CHUNK_SIZE = 64*1024
-
-    outputFile = file
-    fileSize = str(os.path.getsize(file)).zfill(16) #fill left side of string with 0s
+    CHUNK_SIZE = 64 * 1024
+    split = file.split('.')
+    extension = split[len(split)-1]
+    print extension
+    outputFile = "temp."+extension
+    fileSize = str(os.path.getsize(file)).zfill(16)  # fill left side of string with 0s
     IV = ''
+    print fileSize
 
     for i in range(16):
         IV += chr(random.randint(0, 0xFF))
 
-    encryptor = AES.new(key, AES.MODE_CBC, IV)  #chained cypher block
+    encryptor = AES.new(key, AES.MODE_CBC, IV)  # chained cypher block
 
-    #open and read file in binary
+    # open and read file in binary
     with open(file, 'rb') as infile:
         #open and write file in binary
         with open(outputFile, 'wb') as outfile:
-                outfile.write(fileSize)
-                outfile.write(IV)
+            outfile.write(fileSize)
+            outfile.write(IV)
 
+            while True:
+                chunk = infile.read(CHUNK_SIZE)
+
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - (len(chunk) % 16))
+
+                outfile.write(encryptor.encrypt(chunk))
+
+    #copy tmp file to original file name
+        with open(outputFile, 'rb') as infile:
+
+            with open(file, 'wb') as outfile:
                 while True:
                     chunk = infile.read(CHUNK_SIZE)
 
                     if len(chunk) == 0:
                         break
-                    elif len(chunk) % 16 != 0:
-                            chunk += ' ' * (16 - (len(chunk) % 16))
 
-                    outfile.write(encryptor.encrypt(chunk))
+                    outfile.write(chunk)
+
+                fileSize = os.path.getsize(file)
+                #outfile.truncate(fileSize)
+
+        #remove tmp file
+        os.remove(outputFile)
 
 
 def decrypt(key, file):
-    CHUNK_SIZE = 64*1024
-    outputFile = file
+    CHUNK_SIZE = 64 * 1024
+    split = file.split('.')
+    extension = split[len(split)-1]
+    print extension
+    outputFile = "temp."+extension
 
     with open(file, 'rb') as infile:
-            fileSize = long(infile.read(16))
-            print fileSize
-            IV = infile.read(16)
+        fileSize = long(infile.read(16))
+        print fileSize
+        IV = infile.read(16)
 
-            decryptor = AES.new(key, AES.MODE_CBC, IV)
+        decryptor = AES.new(key, AES.MODE_CBC, IV)
 
-            with open(outputFile, 'wb') as outfile:
+        with open(outputFile, 'wb') as outfile:
+            while True:
+                chunk = infile.read(CHUNK_SIZE)
+
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - (len(chunk) % 16))
+                
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(fileSize)  # truncates all padding added at encryption
+
+        #copy tmp file to original file name
+        with open(outputFile, 'rb') as infile:
+
+            with open(file, 'wb') as outfile:
                 while True:
                     chunk = infile.read(CHUNK_SIZE)
 
                     if len(chunk) == 0:
                         break
 
+                    outfile.write(chunk)
 
-                    outfile.write(decryptor.decrypt(chunk))
+                #fileSize = os.path.getsize(file)
+                outfile.truncate(fileSize)
 
-                outfile.truncate(fileSize)  #truncates all padding added at encryption
+        #remove tmp file
+        os.remove(outputFile)
+
 
 def get_key(password):
     hasher = SHA256.new(password)
     return hasher.digest()
 
+
 def main():
     s = sys.argv
-    #s.remove(s[0]) #remove script name
+    # s.remove(s[0]) #remove script name
 
     #Help
     if str.lower(s[1]) == "-h":
@@ -84,7 +128,7 @@ def main():
         file_or_directory = raw_input("Enter a directory or file name: ")
         if not os.path.isdir(file_or_directory):
             if os.path.isfile(file_or_directory):
-                print "-->File" #test
+                print "-->File"  #test
                 print "WARNING: Ensure that you write down and remember this for decryption purposes!"
                 password = raw_input("Enter a password: ")
                 encrypt(get_key(password), file_or_directory)
@@ -93,8 +137,8 @@ def main():
                 print "Error: File entered does not exist! Exiting..."
                 sys.exit(1)
 
-        else: #is a directory
-            print "-->Directory" #test
+        else:  #is a directory
+            print "-->Directory"  #test
             if os.path.isdir(file_or_directory):
                 print "WARNING: Ensure that you write down and remember this for decryption purposes!"
                 password = raw_input("Enter a password: ")
@@ -112,7 +156,7 @@ def main():
         file_or_directory = raw_input("Enter a directory or file name: ")
         if not os.path.isdir(file_or_directory):
             if os.path.isfile(file_or_directory):
-                print "-->File" #test
+                print "-->File"  #test
                 print "WARNING: Ensure that you enter in the exact password that you encrypted with!"
                 password = raw_input("Enter password: ")
                 decrypt(get_key(password), file_or_directory)
@@ -121,8 +165,8 @@ def main():
                 print "Error: File entered does not exist! Exiting..."
                 sys.exit(1)
 
-        else: #is a directory
-            print "-->Directory" #test
+        else:  #is a directory
+            print "-->Directory"  #test
             if os.path.isdir(file_or_directory):
                 print "WARNING: Ensure that you enter in the exact password that you encrypted with!"
                 password = raw_input("Enter password: ")
@@ -141,6 +185,7 @@ def main():
         sys.exit(1)
 
     sys.exit()
+
 
 if __name__ == "__main__":
     main()

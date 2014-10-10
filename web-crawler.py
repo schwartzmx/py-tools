c18ed9f772763103eb7223af925c500ebcf9ce21
Phil Schwartz <schwartzmx@gmail.com>
2014-10-09 22:54:40 -0500

    It's a crawlin'

--- web-crawler.py
+++ web-crawler.py
@@ -5,8 +5,40 @@ __author__ = 'Phil'
 #Crawl the specified webpage.
 
 import sys
-import os.path
 import requests
 from bs4 import BeautifulSoup
 
-def spider()
+bsoup = None
+
+def bs_url(l, url):
+    global bsoup
+    r = requests.get(url)
+    data = r.text
+    bsoup = BeautifulSoup(data)
+    spider(l,url)
+
+def spider(l,url):
+    global bsoup
+    http = 'http'
+    for link in bsoup.find_all('a', href=True):
+        linktext = link['href']
+        if http in linktext:
+            l.append(linktext)
+            print linktext
+        else:
+            linktext = url+linktext
+            l.append(linktext)
+            print linktext
+
+
+    for link in l:
+        l.remove(link)
+        bs_url(l,link)
+
+
+url = sys.argv[1]
+url = "http://"+url
+l = []
+bs_url(l,url)
+
+sys.exit()

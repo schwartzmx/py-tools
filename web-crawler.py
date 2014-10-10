2426e2423d14093330561ed07d7ac8e89f6cbbc5
Phil Schwartz <schwartzmx@gmail.com>
2014-10-09 23:30:50 -0500

    Cleaned up and added argparse

--- web-crawler.py
+++ web-crawler.py
@@ -7,8 +7,33 @@ __author__ = 'Phil'
 import sys
 import requests
 from bs4 import BeautifulSoup
+import argparse
+
 
 bsoup = None
+crawled_links = 1
+max_crawl = None
+
+def main():
+    global max_crawl
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="Specify a URL (Example: www.mysite.com)", action ="store", type=str)
+    parser.add_argument("maxLinksToCrawl", help="Specify the max amount of links to crawl. (100)", action = "store", type=int)
+    args = parser.parse_args()
+
+    if args.url and args.maxLinksToCrawl:
+        max_crawl = args.maxLinksToCrawl
+        url = args.url
+        if not "http" in url:
+            url = "http://"+url
+        l = []
+        bs_url(l,url)
+
+        sys.exit()
+
+    else:
+        print  parser.usage
+
 
 def bs_url(l, url):
     global bsoup
@@ -19,26 +44,32 @@ def bs_url(l, url):
 
 def spider(l,url):
     global bsoup
+    global crawled_links, max_crawl
     http = 'http'
     for link in bsoup.find_all('a', href=True):
         linktext = link['href']
         if http in linktext:
             l.append(linktext)
-            print linktext
+            #print linktext
         else:
             linktext = url+linktext
             l.append(linktext)
-            print linktext
+            #print linktext
 
 
     for link in l:
         l.remove(link)
+        print "Crawled Links:", crawled_links , "<> Current Link: "+link
+
+        if crawled_links == max_crawl:
+            print "Max crawls reached.  \nExited."
+            sys.exit(0)
+
+        crawled_links += 1
         bs_url(l,link)
 
 
-url = sys.argv[1]
-url = "http://"+url
-l = []
-bs_url(l,url)
 
-sys.exit()
+if __name__ == "__main__":
+    main()
+

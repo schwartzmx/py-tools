732914a64f9426a9146d4037aaf9567f6152f62b
Phil Schwartz <schwartzmx@gmail.com>
2014-11-20 15:03:38 -0600

    Changes to use argparser, and also stores password used, just incase password is forgotten. Compared with password before attempting to decrypt.  Various other additions/modifications.

--- cryptdir.py
+++ cryptdir.py
@@ -6,201 +6,237 @@ __author__ = 'Phil'
 import sys
 import os
 import random
+import argparse
 
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 
 
 def encrypt(key, file):
-    CHUNK_SIZE = 64 * 1024
-    split = file.split('.')
-    extension = split[len(split)-1]
+    if key != 1:
+        CHUNK_SIZE = 64 * 1024
+        split = file.split('.')
+        extension = split[len(split)-1]
 
-    #Skip DS_Store as it causes problems when encrypting/decrypting multiple times
-    if extension == "DS_Store":
-        return
+        #Skip DS_Store as it causes problems when encrypting/decrypting multiple times
+        if extension == "DS_Store" or extension == "cd":
+            return
 
-    outputFile = "temp."+extension
-    fileSize = str(os.path.getsize(file)).zfill(16)  # fill left side of string with 0s
-    IV = ''
-    #print fileSize
+        outputFile = "temp."+extension
+        fileSize = str(os.path.getsize(file)).zfill(16)  # fill left side of string with 0s
+        IV = ''
+        #print fileSize
 
-    for i in range(16):
-        IV += chr(random.randint(0, 0xFF))
+        for i in range(16):
+            IV += chr(random.randint(0, 0xFF))
 
-    encryptor = AES.new(key, AES.MODE_CBC, IV)  # chained cypher block
+        encryptor = AES.new(key, AES.MODE_CBC, IV)  # chained cypher block
 
-    # open and read file in binary
-    with open(file, 'rb') as infile:
-        #open and write file in binary
-        with open(outputFile, 'wb') as outfile:
-            outfile.write(fileSize)
-            outfile.write(IV)
+        # open and read file in binary
+        with open(file, 'rb') as infile:
+            #open and write file in binary
+            with open(outputFile, 'wb') as outfile:
+                outfile.write(fileSize)
+                outfile.write(IV)
 
-            while True:
-                chunk = infile.read(CHUNK_SIZE)
-
-                if len(chunk) == 0:
-                    break
-                elif len(chunk) % 16 != 0:
-                    chunk += ' ' * (16 - (len(chunk) % 16))
-
-                outfile.write(encryptor.encrypt(chunk))
-
-    #copy tmp file to original file name
-        with open(outputFile, 'rb') as infile:
-
-            with open(file, 'wb') as outfile:
                 while True:
                     chunk = infile.read(CHUNK_SIZE)
 
                     if len(chunk) == 0:
                         break
+                    elif len(chunk) % 16 != 0:
+                        chunk += ' ' * (16 - (len(chunk) % 16))
 
-                    outfile.write(chunk)
+                    outfile.write(encryptor.encrypt(chunk))
 
-        #remove tmp file
-        os.remove(outputFile)
+            #copy tmp file to original file name
+            with open(outputFile, 'rb') as infile:
+
+                with open(file, 'wb') as outfile:
+                    while True:
+                        chunk = infile.read(CHUNK_SIZE)
+
+                        if len(chunk) == 0:
+                           break
+
+                        outfile.write(chunk)
+
+            #remove tmp file
+            os.remove(outputFile)
+    else:
+        print "Unable to Encrypt."
 
 
 def decrypt(key, file):
-    CHUNK_SIZE = 64 * 1024
-    split = file.split('.')
-    extension = split[len(split)-1]
+    if key != 1:
+        CHUNK_SIZE = 64 * 1024
+        split = file.split('.')
+        extension = split[len(split)-1]
 
-    #Skip DS_STORE as it causes problems when encrypting/decrypting multiple times
-    if extension == "DS_Store":
-        return
+        #Skip DS_STORE as it causes problems when encrypting/decrypting multiple times
+        if extension == "DS_Store" or extension == "cd":
+            return
 
 
-    outputFile = "temp."+extension
+        outputFile = "temp."+extension
 
-    with open(file, 'rb') as infile:
-        fileSize = long(infile.read(16))
-        #print fileSize
-        IV = infile.read(16)
+        with open(file, 'rb') as infile:
+            fileSize = long(infile.read(16))
 
-        decryptor = AES.new(key, AES.MODE_CBC, IV)
+            IV = infile.read(16)
 
-        with open(outputFile, 'wb') as outfile:
-            while True:
-                chunk = infile.read(CHUNK_SIZE)
+            decryptor = AES.new(key, AES.MODE_CBC, IV)
 
-                if len(chunk) == 0:
-                    break
-                elif len(chunk) % 16 != 0:
-                    chunk += ' ' * (16 - (len(chunk) % 16))
-
-                outfile.write(decryptor.decrypt(chunk))
-
-            outfile.truncate(fileSize)  # truncates all padding added at encryption
-
-        #copy tmp file to original file name
-        with open(outputFile, 'rb') as infile:
-
-            with open(file, 'wb') as outfile:
+            with open(outputFile, 'wb') as outfile:
                 while True:
                     chunk = infile.read(CHUNK_SIZE)
 
                     if len(chunk) == 0:
                         break
+                    elif len(chunk) % 16 != 0:
+                        chunk += ' ' * (16 - (len(chunk) % 16))
 
-                    outfile.write(chunk)
+                    outfile.write(decryptor.decrypt(chunk))
 
-                #fileSize = os.path.getsize(file)
-                outfile.truncate(fileSize)
+                outfile.truncate(fileSize)  # truncates all padding added at encryption
 
-        #remove tmp file
-        os.remove(outputFile)
+            #copy tmp file to original file name
+            with open(outputFile, 'rb') as infile:
+
+                with open(file, 'wb') as outfile:
+                    while True:
+                        chunk = infile.read(CHUNK_SIZE)
+
+                        if len(chunk) == 0:
+                            break
+
+                        outfile.write(chunk)
+
+                    #fileSize = os.path.getsize(file)
+                    outfile.truncate(fileSize)
+
+            #remove tmp file
+            os.remove(outputFile)
+    else:
+        print "Unable to Decrypt."
 
 
-def get_key(password):
+def get_key(password, path, eod):
     hasher = SHA256.new(password)
-    return hasher.digest()
+
+    if eod == "encrypt" and os.path.exists(path+'.cd'):
+        os.remove(path+'.cd')
+
+
+    compare = pass_cmp(password, path)
+
+    if compare == True:
+        return hasher.digest()
+    else:
+        print "Invalid Password entered!"
+        sys.exit(1)
+
+def pass_cmp(password, path):
+    if path[:-1] != '/':
+        path = path+'/'
+
+    if os.path.exists(path+'.cd'):
+        with open(path+'.cd', 'r') as efile:
+            p = efile.readline()
+            if password == p:
+                return True
+            else:
+                return False
+    else:
+        with open(path+'.cd', 'w') as nfile:
+            nfile.write(str(password))
+            nfile.close()
+
+        return True
 
 
 def main():
-    s = sys.argv
-    # s.remove(s[0]) #remove script name
+    parser = argparse.ArgumentParser(description="Encrypt/Decrypt a single file or entire directory (along w/ subdirectories) using AES Chained Block Cypher."
+        "\n Specifics: Uses a '.cd' file, within the directory or file directory to log your password and compare for decryption purposes. This could be later changed.")
+    parser.add_argument("file", help="file or directory")
+    parser.add_argument("-e", action="store_true", help="encrypt")
+    parser.add_argument("-d", action="store_true", help="decrypt")
+    args = parser.parse_args()
 
-    #Help
-    if str.lower(s[1]) == "-h":
-        print "Encrypt/Decrypt a File or Entire Directory."
-        print "Encrypt: User$ python cryptdir.py -e"
-        print "Decrypt: User$ python cryptdir.py -d"
+    if args.file:
+        fpath = args.file.split('/')
+        apath = fpath[:-1]
+        path = ''
+        i = 0
+        while i < len(apath):
+            path = path + apath[i]+'/'
+            i += 1
 
     #Encrypt
-    elif str.lower(s[1]) == "-e":
-        file_or_directory = raw_input("Enter a directory or file name: ")
-        if not os.path.isdir(file_or_directory):
-            if os.path.isfile(file_or_directory):
+    if args.e:
+        if not os.path.isdir(args.file):
+            if os.path.isfile(args.file):
                 #print "-->File"  #test
                 print "WARNING: Ensure that you write down and remember this for decryption purposes!"
                 password = raw_input("Enter a password: ")
-                encrypt(get_key(password), file_or_directory)
+                encrypt(get_key(password, path, 'encrypt'), args.file)
                 print "Encryption completed."
             else:
                 print "Error: File entered does not exist! Exiting..."
-                sys.exit(1)
+
 
         else:  #is a directory
             #print "-->Directory"  #test
-            if os.path.isdir(file_or_directory):
+            if os.path.isdir(args.file):
                 print "WARNING: Ensure that you write down and remember this for decryption purposes!"
                 password = raw_input("Enter a password: ")
                 print "Encrypting..."
-                for subdir, dirs, files in os.walk(file_or_directory):
+                for subdir, dirs, files in os.walk(args.file):
                     if subdir:
                         print "   >"+subdir
                         for file in files:
                             print "     --"+file
-                            encrypt(get_key(password), os.path.join(file_or_directory, subdir, file))
-
+                            encrypt(get_key(password, args.file, 'encrypt'), os.path.join(args.file, subdir, file))
                 print "Encryption completed."
 
             else:
                 print "Error: File or Directory does not exist! Exiting..."
-                sys.exit(1)
+
 
     #Decrypt
-    elif str.lower(s[1]) == "-d":
-        file_or_directory = raw_input("Enter a directory or file name: ")
-        if not os.path.isdir(file_or_directory):
-            if os.path.isfile(file_or_directory):
-                #print "-->File"  #test
+    elif args.d:
+        if not os.path.isdir(args.file):
+            if os.path.isfile(args.file):
                 print "WARNING: Ensure that you enter in the exact password that you encrypted with!"
                 password = raw_input("Enter password: ")
-                decrypt(get_key(password), file_or_directory)
+                decrypt(get_key(password, path, False), args.file)
                 print "Decryption completed."
             else:
                 print "Error: File entered does not exist! Exiting..."
-                sys.exit(1)
+
 
         else:  #is a directory
-            #print "-->Directory"  #test
-            if os.path.isdir(file_or_directory):
+            if os.path.isdir(args.file):
                 print "WARNING: Ensure that you enter in the exact password that you encrypted with!"
                 password = raw_input("Enter password: ")
                 print "Decrypting..."
-                for subdir, dirs, files in os.walk(file_or_directory):
+                for subdir, dirs, files in os.walk(args.file):
                     if subdir:
                         print "   >"+subdir
                         for file in files:
                             print "     --"+file
-                            decrypt(get_key(password), os.path.join(file_or_directory, subdir, file))
+                            decrypt(get_key(password, args.file, True), os.path.join(args.file, subdir, file))
 
                 print "Decryption completed."
 
             else:
                 print "Error: File or Directory does not exist! Exiting..."
-                sys.exit(1)
 
-    #None
     else:
-        print "No option entered! Exiting..."
-        sys.exit(1)
+        print parser.usage
 
-    sys.exit()
+
+
 
 
 if __name__ == "__main__":
